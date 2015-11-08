6ed55bc7a7a9042e51870979ffcb73e1e4cbbed8
katie <kjo@live.ca>
2015-11-08 19:51:10 +0100

    Remove onClick listener

--- EpicCamera/app/src/main/java/com/example/katie/epiccamera/LiveCameraFragment.java
+++ EpicCamera/app/src/main/java/com/example/katie/epiccamera/LiveCameraFragment.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 /**
  * Created by katie on 2015-11-08.
  */
-public class LiveCameraFragment extends Fragment implements View.OnClickListener {
+public class LiveCameraFragment extends Fragment {
 
 
     private View mView;
@@ -27,33 +27,6 @@ public class LiveCameraFragment extends Fragment implements View.OnClickListener
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.live_camera, container, false);
         return mView;
-    }
-
-    @Override
-    public void onClick(View view)
-    {
-        /*
-        On click: replace current (LiveCameraFragment) fragment with a PhotoFragment
-         */
-
-        // Create new fragment and transaction
-//        Fragment newFragment = new PhotoFragment();
-//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-//
-//        // Replace whatever is in the livecamera view with this fragment,
-//        // and add the transaction to the back stack
-//        transaction.replace(R.id.livecamera, newFragment);
-////        transaction.remove(R.id.livecamera);
-//        transaction.addToBackStack(null);
-//
-//        // Commit the transaction
-//        transaction.commit();
-
-        mText=(TextView) mView.findViewById(R.id.textview1);
-        mText.setText("@string/frag2");
-        mText.setBackgroundColor(0);
-
-
     }
 
 
