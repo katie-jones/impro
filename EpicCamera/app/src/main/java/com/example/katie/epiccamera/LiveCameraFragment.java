7d8d526dbccaf69da5febb226174fa82097f0603
katie <kjo@live.ca>
2015-11-08 13:49:09 +0100

    Add LiveCameraFragment to app

---
+++ EpicCamera/app/src/main/java/com/example/katie/epiccamera/LiveCameraFragment.java
@@ -0,0 +1,22 @@
+package com.example.katie.epiccamera;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by katie on 2015-11-08.
+ */
+public class LiveCameraFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.live_camera, container, false);
+    }
+
+
+}
