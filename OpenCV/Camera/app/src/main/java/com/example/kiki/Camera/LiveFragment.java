c51f01c8f35b63d53ca8bfa75eb3fa5fb9837917
duembgen <frederike.duembgen@epfl.ch>
2015-11-17 19:15:48 +0100

    fragments with one nested fragment set up and working! :)

--- OpenCV/Camera/app/src/main/java/com/example/kiki/Camera/LiveFragment.java
+++ OpenCV/Camera/app/src/main/java/com/example/kiki/Camera/LiveFragment.java
@@ -1,19 +1,15 @@
 package com.example.kiki.Camera;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * Created by kiki on 10.11.15.
  */
-public class MainFragment extends Fragment {
+public class LiveFragment extends Fragment {
     private View mView; // View corresponding to fragment -- inflated xml file
 
 
@@ -21,22 +17,11 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.mainfragment, container, false);
+        mView = inflater.inflate(R.layout.livefragment, container, false);
 
         return mView;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Fragment stillFragment = new StillFragment();
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.cameraview, stillFragment);
-        transaction.commit();
-
-
-    }
 
 //    @Override
 //    public void onActivityCreated(Bundle savedInstanceState) {
--- OpenCV/Camera/app/src/main/java/com/example/kiki/Camera/MenuFragment.java
+++ OpenCV/Camera/app/src/main/java/com/example/kiki/Camera/MenuFragment.java
@@ -2,15 +2,24 @@ package com.example.kiki.Camera;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by kiki on 10.11.15.
  */
 public class MenuFragment extends PreferenceFragment {
+    private View mView; // View corresponding to fragment -- inflated xml file
+
+
     @Override
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-        view.findViewById(R.id.menufragment);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.menufragment, container, false);
+
+        return mView;
     }
 
 
