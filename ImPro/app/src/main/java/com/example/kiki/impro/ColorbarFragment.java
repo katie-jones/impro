d0828464f0d31b62d2417e02819ea09d709f7686
duembgen <frederike.duembgen@epfl.ch>
2015-12-22 17:08:28 +0100

    Some changes in ColorbarFragment.java and colorbarfragment.xml

--- ImPro/app/src/main/java/com/example/kiki/impro/ColorbarFragment.java
+++ ImPro/app/src/main/java/com/example/kiki/impro/ColorbarFragment.java
@@ -1,11 +1,20 @@
 package com.example.kiki.impro;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
+import org.opencv.core.Range;
+
+import java.util.Set;
 
 
 /**
@@ -14,11 +23,53 @@ import android.view.ViewGroup;
 public class ColorbarFragment extends Fragment {
     static private String TAG="ColorbarFragment";
     private View mView;
-    @Override
+    private RangeSeekBar<Integer> mSeekBar1,mSeekBar2,mSeekBar3;
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         mView = inflater.inflate(R.layout.colorbarfragment, container, false);
+
+        mSeekBar1 = (RangeSeekBar<Integer>) mView.findViewById(R.id.seekbar1);
+        mSeekBar2 = (RangeSeekBar<Integer>) mView.findViewById(R.id.seekbar2);
+        mSeekBar3 = (RangeSeekBar<Integer>) mView.findViewById(R.id.seekbar3);
+
+
+
+        RangeSeekBar.OnRangeSeekBarChangeListener<Integer> listener = new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
+            SharedPreferences mPrefs = getActivity().getSharedPreferences("my",Context.MODE_PRIVATE);
+            //SharedPreferences mPrefs = getActivity().getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor mEditor = mPrefs.edit();
+            Integer test = 0;
+
+            @Override
+            public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
+                RangeSeekBar<Integer> mbar = (RangeSeekBar<Integer>) bar;
+                if (mbar.getId() == mSeekBar1.getId()) {
+                    mEditor.putInt("lower1", minValue);
+                    mEditor.putInt("upper1", maxValue);
+                }
+                if (mbar.getId() == mSeekBar2.getId()) {
+                    mEditor.putInt("lower2", minValue);
+                    mEditor.putInt("upper2", maxValue);
+                }
+                if (mbar.getId() == mSeekBar3.getId()) {
+                    Log.e(TAG,"minValue3: "+ String.valueOf(minValue));
+                    mEditor.putInt("lower3", minValue);
+                    mEditor.putInt("upper3", maxValue);
+                }
+                mEditor.apply();
+                SharedPreferences mPrefs2 = getActivity().getSharedPreferences("my",Context.MODE_PRIVATE);
+                mPrefs2.getInt("lower3", test);
+                Log.e(TAG, "lower3:" + String.valueOf(test));
+            }
+        };
+
+        mSeekBar1.setOnRangeSeekBarChangeListener(listener);
+        mSeekBar2.setOnRangeSeekBarChangeListener(listener);
+        mSeekBar3.setOnRangeSeekBarChangeListener(listener);
+
         return mView;
     }
 
