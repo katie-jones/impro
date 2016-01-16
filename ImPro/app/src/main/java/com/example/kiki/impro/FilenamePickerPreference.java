6b62f0d5bd6c0ceca1b6fce2bb6df920f9ca3f73
duembgen <frederike.duembgen@epfl.ch>
2016-01-16 15:21:32 +0100

    Dynamically added permisisons in Mainactivity and working external ImageSaver

--- ImPro/app/src/main/java/com/example/kiki/impro/FilenamePickerPreference.java
+++ ImPro/app/src/main/java/com/example/kiki/impro/FilenamePickerPreference.java
@@ -6,7 +6,9 @@ import android.graphics.Bitmap;
 import android.media.Image;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.preference.DialogPreference;
+import android.provider.MediaStore;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by katie on 2015-12-07.
@@ -80,19 +83,22 @@ public class FilenamePickerPreference extends DialogPreference {
 
             // start new thread to save picture
             // Make new file in internal storage
-            File file = new File(getContext().getFilesDir(),mFilename+".png");
+            //File file = new File(getContext().getFilesDir(),mFilename+".png");
+
+            //mFilename = getContext().getFilesDir()+"/"+mFilename+".png";
 
             // make a new image saver and run it
-            ImageSaver mImageSaver = new ImageSaver(CommonResources.filteredBitmap,file);
-//            mImageSaver.run();
+            //ImageSaverInternal mImageSaver = new ImageSaverInternal(CommonResources.filteredBitmap,file);
+
+            ImageSaverExternal mImageSaver = new ImageSaverExternal(CommonResources.filteredBitmap,mFilename+".png",getContext());
             Thread mThread = new Thread(mImageSaver,"ImageSavingBadassMofoThread");
             mThread.start();
 
-            // set file world readable
-            file.setReadable(true, false);
+            // set file world readable: Doesn't work for internal storage!
+            // file.setReadable(true, false);
 
             // alert media scanner of new image file
-            MediaScannerConnection.scanFile(getContext(), new String[]{file.toString()}, null, new MediaScannerConnection.OnScanCompletedListener() {
+            MediaScannerConnection.scanFile(getContext(), new String[]{mFilename+".png"}, null, new MediaScannerConnection.OnScanCompletedListener() {
                 public void onScanCompleted(String path, Uri uri) {
                     Log.i(TAG, "Scanned " + path + ":");
                     Log.i(TAG, "uri=" + uri);
@@ -107,47 +113,84 @@ public class FilenamePickerPreference extends DialogPreference {
      * Saves a JPEG {@link Image} into the specified {@link File}.
      */
 
-    private static class ImageSaver implements Runnable {
-        //The JPEG image
+    private static class ImageSaverInternal implements Runnable {
         private final Bitmap mImage;
-        //The file we save the image into
         private final File mFile;
 
-        public ImageSaver(Bitmap image, File file) {
+        public ImageSaverInternal(Bitmap image, File file) {
             mImage = image;
             mFile = file;
+            //mContext = context;
         }
 
         @Override
         public void run() {
 
-//                ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
-//                byte[] bytes = new byte[buffer.remaining()];
-//                buffer.get(bytes);
             FileOutputStream output = null;
             try {
-//                    File mFile = getAlbumStorageDir(mFilename);
                 if (mFile.exists()) mFile.delete();
-                mFile.createNewFile();
+                //mFile.createNewFile();
                 Log.e(TAG, "Make file output stream");
                 output = new FileOutputStream(mFile);
                 Log.e(TAG, "Start saving file");
                 mImage.compress(Bitmap.CompressFormat.PNG, 100, output);
-                output.flush();
-                Log.e(TAG, "Image saved! filename:" + mFile.getAbsolutePath());
-//                    output.write(bytes);
+                output.flush(); // makes sure all data in buffer is written
+                Log.e(TAG, "Image saved! filename:" + mFile.getName());
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                    if (null != output) {
-                        try {
-                            output.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                if (null != output) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+                }
             }
+        }
+    }
 
+    public final static String APP_PATH_SD_CARD = "/DesiredSubfolderName/";
+    public final static String APP_THUMBNAIL_PATH_SD_CARD = "thumbnails";
+
+
+
+    private static class ImageSaverExternal implements Runnable {
+        //The JPEG image
+        private final Bitmap mImage;
+        private final String mFilename;
+        private final Context mContext;
+
+        public ImageSaverExternal(Bitmap image, String file, Context context) {
+            mImage = image;
+            mFilename = file;
+            mContext = context;
+        }
+
+        @Override
+        public void run() {
+            String fullPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
+            try {
+                File dir = new File(fullPath);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+
+                OutputStream fOut = null;
+                File file = new File(fullPath,mFilename);
+                file.createNewFile();
+                fOut = new FileOutputStream(file);
+
+                mImage.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                fOut.flush();
+                fOut.close();
+
+                MediaStore.Images.Media.insertImage(mContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
+
+                Log.e(TAG, "Image saved! filename:" + file.getAbsolutePath());
+            } catch (Exception e) {
+                Log.e("saveToExternalStorage()", e.getMessage());
+            }
         }
     }
 }
--- Impro/app/src/main/java/com/example/kiki/impro/MainActivity.java
+++ Impro/app/src/main/java/com/example/kiki/impro/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.kiki.impro;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,6 +39,38 @@ public class MainActivity extends Activity implements MainFragment.MainInterface
     private static final String TAG_STILL_FRAGMENT = "still_fragment";
     private static final String TAG_COLORBAR_FRAGMENT = "colorbar_fragment";
     private static boolean stillActive;
+
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -78,6 +113,8 @@ public class MainActivity extends Activity implements MainFragment.MainInterface
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
+
+        verifyStoragePermissions(this);
     }
 
     public void onButtonClicked(View v) {
